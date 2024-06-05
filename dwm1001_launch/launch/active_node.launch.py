964b86db56cd5bd2abd67a8cf97d5e875461df43
jattys <jordan.attys@richmond.edu>
2024-06-05 10:43:26 -0400

    dwm changes

--- dwm1001_launch/launch/active_node.launch.py
+++ dwm1001_launch/launch/active_node.launch.py
@@ -28,7 +28,7 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument 
 from launch.substitutions import LaunchConfiguration, TextSubstitution, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
